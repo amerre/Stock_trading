f448dfd907c3b1de2bfa7acaa851d17260a0a0be
amerre <adr.merre@gmail.com>
2019-01-18 08:52:54 +0100

    :bug: Fixing router urls

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,15 +2,15 @@
   <nav class="navbar navbar-default">
     <div class="container-fluid">
       <div class="navbar-header">
-        <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+        <router-link to="/stocktrader" class="navbar-brand">Stock Trader</router-link>
       </div>
 
       <div class="collapse navbar-collapse">
         <ul class="nav navbar-nav">
-          <router-link to="/portfolio" activeClass="active" tag="li">
+          <router-link to="/stocktrader/portfolio" activeClass="active" tag="li">
             <a>Portfolio</a>
           </router-link>
-          <router-link to="/stocks" activeClass="active" tag="li">
+          <router-link to="/stocktrader/stocks" activeClass="active" tag="li">
             <a>Stocks</a>
           </router-link>
         </ul>
